6486b58f79fde8a6a573e30388600b9a3c0ae8a3
jwu611 <jane.wu@hotmail.ca>
2021-05-11 21:53:13 -0700

    updated classifier website

--- classification_website/classification_web.py
+++ classification_website/classification_web.py
@@ -30,6 +30,36 @@ ingredient_dict = {
 
 }
 
+ingredient_id_map = {
+    "apple": 150,
+    "avocado": 255,
+    "banana": 342,
+    "beef": 422,
+    4: "bellpeppers",
+    "bread": 715,
+    "broccoli": 756,
+    "cabbage": 893,
+    "cheese": 1170,
+    "chicken": 1240,
+    "corn": 1803,
+    "cucumber": 2001,
+    "egg": 2499,
+    "eggplant": 2516,
+    "greenbeans": 3398,
+    "lemon": 4231,
+    "lettuce": 4308,
+    "mushroom": 4863,
+    "olives": 5003,
+    "onions": 5010,
+    20: "pasta",
+    "potatoes": 5648,
+    "rice": 6261,
+    "salmon": 6260,
+    "spinach": 6754,
+    "tomato": 7213,
+
+}
+
 #from flask_ngrok import run_with_ngrok
 
 from flask_session.__init__ import Session
@@ -39,6 +69,7 @@ from werkzeug.utils import secure_filename
 import os
 import pickle
 import glob
+import pandas as pd
 import torch, torchvision
 from torch import nn, optim
 from torchvision import datasets, models, transforms
@@ -51,11 +82,18 @@ app = Flask(__name__)
 
 
 
-infile = open('ingredient_classifier.pkl', 'rb')
-model = pickle.load(infile)
+infile = open('ingredient_classifier_torch.pkl', 'rb')
+model = torch.load(infile, map_location=torch.device('cpu'))
 infile.close()
 
+#ingr_map = open('our_ingr_map.pkl', 'rb')
+df = pd.read_pickle('our_ingr_map.pkl')
+#ingr_map.close()
 
+#dict = df[df["replaced"].isin(ingr)][["replaced", "id"]].unique().set_index("replaced")["id"].to_dict()
+#ingr = ['apple', 'avocado', 'banana', 'beef', 'bellpeppers', 'bread', 'broccoli', 'cabbage', 'cheese', 'chicken', 'corn', 'cucumber', 'egg', 'eggplant', 'greenbeans', 'lemon', 'lettuce', 'mushroom', 'olives', 'onions', 'pasta', 'potatoes', 'rice', 'salmon', 'spinach', 'tomato']
+
+#print(df)
 
 app.config["image_uploads"] = 'image-uploads/sample'
 
@@ -68,14 +106,18 @@ def upload_image():
     os.makedirs("./image-uploads/sample", exist_ok=True)
     if request.method == "POST":
         #if request.files:
-            image = request.files["image"]
-            filename = secure_filename(image.filename)
-            path = os.path.join(app.config["image_uploads"], filename)
-            image.save(path)
+            images = request.files.getlist("image[]")
+
+            for i in images:
+              filename = secure_filename(i.filename)
+              path = os.path.join(app.config["image_uploads"], filename)
+              i.save(path)
+
             xform = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor()])
             inputs = datasets.ImageFolder('./image-uploads', transform=xform)
-            loader = torch.utils.data.DataLoader(inputs, batch_size = 4, shuffle=True)
+            loader = torch.utils.data.DataLoader(inputs, batch_size = len(images), shuffle=True)
             model.eval()
+            print("breakpoint")
             with torch.no_grad():
               for samples,_ in loader:
                 samples = samples.to(device)
@@ -83,16 +125,20 @@ def upload_image():
                 _, preds = torch.max(outs.detach(), 1)
             
             preds_list = preds.tolist()
-            #for p in preds_list:
-            #  print(p)
-            print(ingredient_dict[preds_list[0]])
-            print("finished")
-            return render_template("output_prediction.html", ingredient=ingredient_dict[preds_list[0]])
+            
+            ingredients_list = []
+
+            for p in preds_list:
+                ingredients_list.append(ingredient_id_map[ingredient_dict[p]])
+
+
+
+            return render_template("output_prediction.html", ingredients=ingredients_list)
 
     return render_template("uploadImage.html")
 
 @app.route('/output_prediction/<i>')
 def output_prediction(i):
-  return render_template("output_prediction.html", ingredient=i)
+  return render_template("output_prediction.html", ingredients=i)
 
 #app.run()
